faee7dcb69af5ad50189b7ae67164afe80c9de90
re-factor <refactor1010@gmail.com>
2023-11-29 11:37:45 -0400

    refactor: code refactored using re-facto plugin

--- test2.java
+++ test2.java
@@ -2,11 +2,19 @@ public class DirtyCode {
     public static void main(String[] args) {
         int x = 5; int y = 10; boolean flag = true;
         
-        if (x>3 && y<12 || flag == true){ System.out.println("Dirty Code is Fun!"); } 
-        else { System.out.println("Clean Code is Overrated!"); }
+        if (x > 3 && y < 12 || flag){
+            System.out.println("Dirty Code is Fun!");
+        } else {
+            System.out.println("Clean Code is Overrated!");
+        }
         
-        for (int i = 0; i<5; i++){ if (i%2 == 0){ System.out.println("Messy Loops FTW!"); } 
-        else { System.out.println("Who needs indentation anyway?"); } }
+        for (int i = 0; i < 5; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Messy Loops FTW!");
+            } else {
+                System.out.println("Who needs indentation anyway?");
+            }
+        }
         
         System.out.println("This is just the beginning of the chaos!");
     }
