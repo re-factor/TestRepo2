707338e222ae61b4ebf2145ce9ed7e0afbc24022
re-factor <refactor1010@gmail.com>
2023-11-30 12:36:39 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
-def add_numbers(x, y):
-    return x + y
-
+import random
+ 
+def discount(price):
+   magic_number = random.uniform(0.1, 0.3)
+     discounted_price = price - (price * magic_number)
+   return discounted_price
+ 
+def main():
+    p = float(input("Enter the original price: "))
+ 
+    #unutilized variables
+    ab=0.2
+    c=73
+ 
+    dp = discount(p)
+ 
+  print(f"Original Price: ${p:.2f}")
+    print("Discounted Price: $" + str(round(dp, 2)))
+ 
 if __name__ == "__main__":
-    """ main method 1234567 """
-    print(add_numbers(1, 5))
-
-
-def add_numbers(x, y):
-return x + y
-
-if __name__ == "__main__":
-""" main method 1234567 """
-print(add_numbers(1, 5))
+  main()
