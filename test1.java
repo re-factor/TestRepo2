aff0ca0a97879ce831a1c3e51ea875827e814491
re-factor <refactor1010@gmail.com>
2023-11-30 11:08:13 -0400

    Update test1.java

--- test1.java
+++ test1.java
@@ -13,5 +13,12 @@ public class UnrefactoredComplexCode {
         boolean flag1 = true;
         boolean flag2 = false;
         boolean flag3 = true;
+
+        int a = 5;
+        int b = 10;
+        int c = 15;
+        boolean flag1 = true;
+        boolean flag2 = false;
+        boolean flag3 = true;
     }
 }
